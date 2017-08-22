e4503b1bd1b87f0bfc157bfdc45839ac6e4721d8
Raditia Madya <gustiraditia@gmail.com>
2017-08-22 20:17:35 +0700

    Update

---
+++ src/main/java/com/raditia/springjsp/DAO/UserDao.java
@@ -0,0 +1,11 @@
+package com.raditia.springjsp.DAO;
+
+public class UserDao {
+
+    private static final table_name = "user_data";
+    private static final col_id = "id";
+    private static final col_name = "name";
+    private static final col_email = "email";
+
+
+}
