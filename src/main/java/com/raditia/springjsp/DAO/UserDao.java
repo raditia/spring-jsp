ac4aae9719845c8607b1c1cc9da63f2c81574830
Raditia Madya <gustiraditia@gmail.com>
2017-08-22 21:22:31 +0700

    Update

--- src/main/java/com/raditia/springjsp/DAO/UserDao.java
+++ src/main/java/com/raditia/springjsp/DAO/UserDao.java
@@ -1,7 +1,12 @@
 package com.raditia.springjsp.DAO;
 
+import com.raditia.springjsp.model.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
 
@@ -30,5 +35,62 @@ public class UserDao {
         System.out.println("Database opened successfully");
 
         return connection;
+    }
+
+    //Membuat tabel
+    public static void createTableUser() {
+
+        try {
+            Connection connection  = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS " + table_name +
+                            "(id SERIAL PRIMARY KEY NOT NULL, " +
+                            "name TEXT NOT NULL ," +
+                            "email TEXT)"
+            );
+
+            preparedStatement.executeUpdate();
+        }
+        catch (Exception e) {
+
+            System.out.println(e.toString());
+        }
+    }
+
+    //Memasukkan data
+    public static void insertUser(User user) {
+
+        int status = 0;
+
+        try {
+            Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO " + table_name +
+                            "(" + col_name +"," +col_email +") VALUES (?,?)"
+            );
+        }
+        catch (Exception e) {
+
+            System.out.println(e.toString());
+        }
+    }
+
+    //Menampilkan semua user
+    public static List<User> getAllUser() {
+
+        List<User> userList = new ArrayList<User>();
+
+        try {
+            Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM " + table_name
+            );
+        }
+        catch (Exception e) {
+
+            System.out.println(e.toString());
+        }
+
+        return userList;
     }
 }
--- src/main/java/com/raditia/springjsp/SpringjspApplication.java
+++ src/main/java/com/raditia/springjsp/SpringjspApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringjspApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringjspApplication.class, args);
+
 	}
 }
