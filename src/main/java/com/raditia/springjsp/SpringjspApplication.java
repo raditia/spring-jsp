9280ce917c56ee65db0e9a6fa71a57cee402022c
Raditia Madya <gustiraditia@gmail.com>
2017-08-22 20:00:27 +0700

    Initial commit

---
+++ src/main/java/com/raditia/springjsp/SpringjspApplication.java
@@ -0,0 +1,12 @@
+package com.raditia.springjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjspApplication.class, args);
+	}
+}
